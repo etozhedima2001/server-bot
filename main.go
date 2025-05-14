a0bf4130355be4016d990284542220605299e4c1
dimawhy <whydmitriy@gmail.com>
2025-05-14 21:53:43 +0900

    test cicd

--- main.go
+++ main.go
@@ -101,7 +101,7 @@ func main() {
 		case "/start":
 			msg.Text = "Привет! Я бот для управления сервером. Доступные команды:\n/status - проверить сервер\n/cicd - узнать статус ci/cd"
 		case "/status":
-			msg.Text = "Сервер работает!testing deploy"
+			msg.Text = "Сервер работает!"
 		case "/cicd":
 			status, err := getGitHubActionsStatus(repoOwner, repoName, githubToken)
 			if err != nil {
